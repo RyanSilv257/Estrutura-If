3ef27f01ab8fd4725e59e5681b3e192f9aaf7967
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-09-29 08:52:39 -0300

    commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $numero1 = 5;
+    $numero2 = 2;
+
+    $nome1 = "Robson";
+    $nome2 = "Pedro";
+
+    $numero3 = 12;
+    $numero4 = 11;
+
+    if ($numero1 > $numero2) {
+        echo "5 é maior que 2";
+    } else {
+        echo "5 não é maior que 2";
+    }
+    echo "<br>";
+
+    if ($nome1 == $nome2) {
+        echo "Robson é igual a Pedro";
+    } else {
+        echo "Robson não é igual a Pedro";
+    }
+    echo "<br>";
+
+    if ($numero3 <= $numero4) {
+        echo "12 é menor igual a 11";
+    } else {
+        echo "12 não é menor igual a 11";
+    }
+?>

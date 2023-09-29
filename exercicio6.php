56ace4e65555e0643898119e8a103417db5e300c
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-09-29 12:10:05 -0300

    commit

--- exercicio6.php
+++ exercicio6.php
@@ -1,4 +1,14 @@
 <?php 
+    $numeros = array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+
+    foreach($numeros as $value){
+        if ($value == 30 || $value == 40){
+            continue;
+        }
+        echo $value + 1;
+        echo "<br>";
+        
+    }
 
 
 ?>
